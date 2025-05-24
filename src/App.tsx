f9ad06e0c025ee5962066b8c6a9f0015e8a5c5bd
Karan-Sundar <karansundar01@gmail.com>
2025-05-24 21:22:24 +0530

    Update App.tsx (Fix intro video)

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
-import IntroVideo from "./components/Introvideo";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Blog from "./pages/Blog";
-import LMS from "./pages/LMS";
-import Shop from "./pages/Shop";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
+import IntroVideo from "@/components/Introvideo";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
+import Blog from "@/pages/Blog";
+import LMS from "@/pages/LMS";
+import Shop from "@/pages/Shop";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import Profile from "@/pages/Profile";
 
 const queryClient = new QueryClient();
 
